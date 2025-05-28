595c8d9e890c69f929197e9ed4bda9f655b4d441
LucelyMendoza <lmendoza@unsa.edu.pe>
2025-05-28 10:41:10 -0500

    Datos del mapa con canvas

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,60 @@
 import 'package:flutter/material.dart';
+import 'screens/MapaScreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Centro Cultural UNSA',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
+      theme: ThemeData(primarySwatch: Colors.red),
       home: WelcomeScreen(),
     );
   }
 }
 
 class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              Color(0xFFF7ECD8), // color de fondo (F7ECD8)
-              Color(0xFFF7ECD8), // mismo color para hacer el degradado
-            ],
+            colors: [Color(0xFFF7ECD8), Color(0xFFF7ECD8)],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            
-            // Texto de bienvenida
             Text(
               'Bienvenido al Centro Cultural de UNSA',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Color(0xFF84030C), // Color rojo (84030C)
+                color: Color(0xFF84030C),
               ),
             ),
-            
-            // Imagen en el centro
-            Image.asset('assets/logo.png', height: 300), 
-
+            Image.asset('assets/logo.png', height: 300),
             SizedBox(height: 20),
-
-            // Botón "Comenzar recorrido"
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 50),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF84030C), // color rojo
+                  backgroundColor: Color(0xFF84030C),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                   padding: EdgeInsets.symmetric(vertical: 15),
                 ),
                 onPressed: () {
-                  // Navegar al menu principal
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => MenuScreen()),
@@ -73,7 +64,7 @@ class WelcomeScreen extends StatelessWidget {
                   'Comenzar Recorrido',
                   style: TextStyle(
                     fontSize: 18,
-                    color: Color(0xFFF7ECD8), // color amarillo(F7ECD8)
+                    color: Color(0xFFF7ECD8),
                   ),
                 ),
               ),
@@ -86,30 +77,22 @@ class WelcomeScreen extends StatelessWidget {
 }
 
 class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Menú Principal"),
-      ),
+      appBar: AppBar(title: Text("Menú Principal")),
       body: Center(
-        child: Text("Bienvenido al menú principal"),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Perfil',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Configuración',
-          ),
-        ],
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MapaScreen()),
+            );
+          },
+          child: Text("Ver Mapa Interactivo"),
+        ),
       ),
     );
   }
