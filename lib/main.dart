8861659472d5828b6635f7ec73bcbc9a3e07ae2d
LucelyMendoza <lmendoza@unsa.edu.pe>
2025-06-11 11:04:47 -0500

    Uso de hive NoSQL

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/post_provider.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/posts_screen.dart'; 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'views/welcome.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => PostProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Centro Cultural UNSA',
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: WelcomeScreen(),
-        routes: {
-          '/posts': (context) => PostsScreen(),
-        },
+    return MaterialApp(
+      title: 'Centro Cultural UNSA',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
+      home: const Welcome(),
     );
   }
 }
